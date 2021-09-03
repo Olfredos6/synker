ac653ad040dd9f93123e57acc50a95e76106c0d9
Olfredos <alfred.nehemie@gmail.com>
2021-09-03 05:47:42 +0000

    cleanup online, moving local

--- src/synker/runner.py
+++ src/synker/runner.py
@@ -1,4 +1,7 @@
 from time import sleep
+from . import settings
+from . import github
+import os
 
 
 def run():
@@ -6,14 +9,42 @@ def run():
         Main function.
         Runs sync function every 5 minutes
     '''
-    while True:
-        sync()
-        sleep(1000)
+    check_data()
+    # while True:
+    #     sync()
+    #     sleep(60)
 
+
+def check_data():
+    '''
+        Make sure all the required files are ready.
+        If not, create them.
+    '''
+
+    DIR_DATA_EXISTED = True
+    if not os.path.exists(settings.DATA_DIR):
+        DIR_DATA_EXISTED = False
+        os.mkdir(settings.DATA_DIR)
+    
+    if not DIR_DATA_EXISTED:
+        # create the current user's data file
+        user_data = github.get_current_user_info()
+        print(user_data, dir(user_data))
+
+
+
+def sync_user_data():
+    github.get_current_user_info()
 
 
 def sync():
     '''
 
     '''
-    print("syncing...")
+    # check_data()
+    # print("syncing...")
+    # sync_user_data()
+    # repos = github.collect_repos()
+    # for r in repos:
+    #     print(r)
+    # print(settings.API_URL)
